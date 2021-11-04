6fc2eb95fb8076566a82fac86f5d6ee605257e23
joaquin <jecorrea@protonmail.com>
2021-11-04 17:10:03 -0400

    javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+// Selecting panels
+const panels = document.querySelector(".panel");
+
+panels.forEach((panel) => {
+  // console.log(panel)
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
